788972b69eb1ca8b6a9ca862d751470f80b9aa25
RyuHyunJung <83815677+RyuHyunJung@users.noreply.github.com>
2021-05-09 15:39:30 +0900

    test

---
+++ 3.py
@@ -0,0 +1,15 @@
+# 여러줄 주석  Shift + Alt + A
+# 한줄한줄 Ctrl + /
+
+x = "Hello"                   
+y = 'Bye'                     
+z = """안녕하세요.            
+저는 21학번 새내기입니다"""    
+
+print(x)
+print(y)
+print(z)
+print("\n")                   # 공백 행 
+print(x+y+z)
+print("\n")                   # 공백 행 
+print(z)
